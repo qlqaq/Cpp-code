87830bc75c41ad56c082b29e63fcfd8c5a980873
qlqaq <Priority_Deque@outlook.com>
2025-08-28 19:05:24 +0800

    update

---
+++ Luogu P3919.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl "\n"
+using namespace std;
+//const int MAXN=;
+//int maxn=INT_MIN;
+//int minn=INT_MAX;
+int n,m,a[1000005];
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    for(int i=1; i<=n; i++) cin >> a[i];
+    while(m--) {
+        int v,op,p,c;
+        cin >> v >> op >> p;
+        if(op==1) {
+            cin >> c;
+
+        }
+        else {
+            
+        }
+    }
+    return 0;
+}
